168ca6b12ea62f221339e8574be87601afce5145
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-10 18:10:33 +0530

    day13 dot net learning problem

---
+++ day13 uc L.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Generics
+{
+    class program
+    {
+        public static void toprint(int[] inputArray)
+
+        {
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("----------------------");
+        }
+
+        public static void toprint(double[] inputArray)
+
+        {
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("----------------------");
+        }
+        public static void toprint(char[] inputArray)
+        {
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("----------------------");
+        }
+        static void main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, 6 }
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5,);
+            char[] charArray = { 'h', 'e', 'l', 'l', 'o' };
+
+            program.toprint(intArray);
+            program.toprint(doubleArray);
+            program.toprint(charArray);
+        }
+    }
+
+}
+
+           program.toprint(
+
+
+
+
+    }
+}
+    }
+}
+
