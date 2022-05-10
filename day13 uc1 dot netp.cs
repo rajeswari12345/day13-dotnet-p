732ce73ebff9b73c695322930812b2f2ebabd7d2
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-10 18:11:22 +0530

    day13 uc1

---
+++ day13 uc1 dot netp.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System. Collections.Generic;
+
+namespace MaximumIntegerNumber;
+public static int MaximumIntegerNumber(int firstvalue, int secondvalue, int thirdvalue)
+{
+    if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+    if (firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+    if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0 
+    {
+                return firstvalue;
+     }
+    if(secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+     if(secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0||
+     if(secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0
+     {
+                return secondvalue; 
+                    
+      }
+    if(thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+     if(thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+     if(thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0
+     {
+                return thirdvalue;
+      }
+    return firstvalue;
+}
