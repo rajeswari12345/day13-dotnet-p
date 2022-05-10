457d53f0b18a945db02f65484c604c2c77aae1c3
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-10 18:15:03 +0530

    day13 uc4

---
+++ day13 uc4 dotnet.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+public class GenericMaximum<T> where T : IComparable
+{
+    public T[] value;
+    public GenericMaximum(T[] Value)
+    {
+        this.value = Value;
+    }
+    public T[] sort(T[] values)
+    {
+        Array.Sort(values);
+        return values;
+    }
+    public T Maxmethod()
+    {
+        var max = maxvalue(this.value);
+        return max;
+    }
+    public void printmaxvalue()
+    {
+        var max = maxvalue(this.value);
+        Console.WriteLine(" maximum value is " + max);
+    }
+    static void main(string[] args)
+    {
+        int[] arr = { 112, 234, 564, 666, 698);
+        GenericMaximum<int> generic = new GenericMaximum<int>(arr);
+        generic.printmaxvalue();
+    }
+
