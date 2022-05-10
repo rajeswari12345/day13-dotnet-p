379e6896ae018b52078e1834af98c2710c6ebe98
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-10 18:12:11 +0530

    day13  uc2

---
+++ day13 uc2 dot net p.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System. Collections.Generic;
+
+namespace MaximumFloatNumber;
+public static int MaximumFloatNumber(double firstvalue, double secondvalue, double thirdvalue)
+{
+    if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+    if (firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+    if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >= 0
+    {
+                return firstString;
+            }
+    if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+     if (secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+     if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >= 0
+     {
+                return secondString;
+
+            }
+    if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+     if (thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+     if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >= 0
+     {
+                return thirdvalue;
+            }
+    return firstvalue;
+}
